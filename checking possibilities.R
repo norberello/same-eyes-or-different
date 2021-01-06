8be105f64142b7d7721ab67db0e775895dd040c8
Norber <norberello@gmail.com>
2021-01-06 21:28:40 +0100

    folder plot

--- checking possibilities.R
+++ checking possibilities.R
@@ -1,3 +1,27 @@
 library(openintro)
 str(assortive_mating)
+?assortive.mating
+tab.partners<-table(assortive_mating$self_male,assortive_mating$partner_female)
+tab.partners
+x.out<-chisq.test(tab.partners)
+chisq.test(assortive_mating$self_male,assortive_mating$partner_female)
+#same result
+
+library(ggplot2)
+ggplot(assortive_mating, aes(x = self_male, fill = partner_female)) + 
+  geom_bar()
+
+#great analyses and plotting in here:
+#https://rcatlord.github.io/GSinR/modules/model/exercises/model_data.html
+#check library gmodels
+
+install.packages("gmodels")
+library(gmodels)
+CrossTable(assortive_mating$self_male,assortive_mating$partner_female, prop.t = FALSE, 
+           prop.r = FALSE, prop.c = FALSE, expected = TRUE, resid = TRUE, sresid = TRUE, 
+           format = "SPSS")
+
+library("corrplot")
+corrplot(x.out$stdres, type="upper",
+         is.corr = FALSE)
 
